6b36b7c74800177d7d83e042940cafbe5b1d3d59
Christian Hoyt <cthoyt11@gmail.com>
2020-07-07 18:31:18 -0700

    Made new R file  to address strange ACF title cutoff, added weather data, joined to NYC data

--- charts_alt.R
+++ charts_alt.R
@@ -1,12 +1,5 @@
----
-title: "Charts_for_2020_06_09"
-output: html_document
----
-
-```{r message=FALSE, echo=FALSE}
+setwd('~/Learning/stat-626-project')
 # Load the data
-defaultW <- getOption("warn") #turn off the warnings
-options(warn = -1) #turn off all warnings
 library(readr)
 library(tidyverse)
 library(astsa)
@@ -29,31 +22,23 @@ ridership <- read_csv("trips_data_by_day.csv",
 
 weather  <- read_csv("noaa_weather_central_park.csv")
 
-str(weather)
-```
-
-```{r}
 nyc_ridership <- ridership %>% 
   filter(city == "NYC")%>%
   left_join(weather, by = c("trip_day"="DATE")) %>%
   mutate(
-    nyc_ridership$ridership_average_7 = stats::filter(nyc_ridership$trip_count, sides=2, filter=rep(1/7,7))
-    , nyc_ridership$ridership_average_31 = stats::filter(nyc_ridership$trip_count, sides=2, filter=rep(1/31,31))
+    ridership_average_7 = stats::filter(nyc_ridership$trip_count, sides=2, filter=rep(1/7,7))
+    , ridership_average_31 = stats::filter(nyc_ridership$trip_count, sides=2, filter=rep(1/31,31))
   )
 
-```
-
-```{r}
 ggplot(data = nyc_ridership, mapping=aes(x=trip_day)) +
   geom_line(mapping = aes(y=trip_count), color = "orange") +
   geom_line(mapping =aes(y=ridership_average_31), color= "blue") +
   ggtitle("NYC Daily Citibike Trip Counts with 31 Day Moving Average") +
   scale_y_continuous(label=comma) +
-  theme(plot.title = element_text(hjust=0.5, color = "white"),
+  theme(plot.title = element_text(hjust=0.5),
         axis.title = element_blank()
   )
-```
-```{r}
+
 ggplot(data = nyc_ridership, mapping=aes(x=trip_day)) +
   geom_line(mapping = aes(y=trip_count), color = "orange") +
   geom_line(mapping =aes(y=ridership_average_7), color= "blue") +
@@ -62,10 +47,7 @@ ggplot(data = nyc_ridership, mapping=aes(x=trip_day)) +
   theme(plot.title = element_text(hjust=0.5),
         axis.title = element_blank()
   )
-```
 
-
-```{r}
 nyc_ridership$log_trip_count <- log(nyc_ridership$trip_count)  #y = log transformation
 
 ggplot(data = nyc_ridership, mapping=aes(x=trip_day)) +
@@ -74,13 +56,10 @@ ggplot(data = nyc_ridership, mapping=aes(x=trip_day)) +
   # scale_y_continuous(label=percent) +
   theme(plot.title = element_text(hjust=0.5),
         axis.title = element_blank())
-```
 
-```{r}
 attach(nyc_ridership)
 differenced_log_trip_count <- diff(log_trip_count)
 row_count <- length(log_trip_count)
-
 
 ggplot(mapping=aes(x=trip_day[2:row_count])) +
   geom_line(mapping=aes(y=diff(log_trip_count)), color= "blue") +
@@ -89,50 +68,22 @@ ggplot(mapping=aes(x=trip_day[2:row_count])) +
         axis.title = element_blank())
 
 detach()
-```
 
-
-```{r}
 acf_differenced_log_trip_count = acf(differenced_log_trip_count, lag.max = 36, plot = FALSE)
+plot(acf_differenced_log_trip_count, main = "ACF of Transformed Bike Trips")
 
-plot(acf_differenced_log_trip_count, main = "TEST")
-
-par(mfrow=c(1,1))
-```
-```{r}
 attach(nyc_ridership)
-
 tsplot(x=trip_day, y=trip_count/1000, main="NYC Total Daily Trips: KSmooth Transformation", col="orange", lwd=2, ylab = "Number of Trips/1000")
 lines(ksmooth(x=trip_day, y=trip_count/1000, "normal", bandwidth = 12), col="blue", lwd=2)
-
 detach()
-```
 
-```{r}
 pacf_differenced_log_trip_count = pacf(differenced_log_trip_count, lag.max = 36)
-```
+pacf_log_trip_count <- pacf(nyc_ridership$log_trip_count, lag.max = 36, plot = FALSE)
+plot(pacf_log_trip_count, main = "PACF of Transformed Bike Trips")
 
-```{r}
-pacf_log_trip_count <- pacf(nyc_ridership$log_trip_count, lag.max = 36)
-```
-
-
-```{r}
-attach(nyc_ridership)
-
-tsplot(x=trip_day, y=trip_count/1000, main="NYC Total Daily Trips: KSmooth Transformation", col="orange", lwd=2, ylab = "Number of Trips/1000")
-lines(ksmooth(x=trip_day, y=trip_count/1000, "normal", bandwidth = 12), col="blue", lwd=2)
-
-detach()
-```
-
-```{r}
 attach(nyc_ridership)
 trips.yw <- ar.yw(trip_day, order=7)
 nyc_ts <- ts(nyc_ridership)
 tsplot(y=trip_count, x=trip_day, col="orange", main="Yule Walker Series")
-
 detach()
-```
-
 
